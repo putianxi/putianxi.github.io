7965e3929e35618201c0ba5fa22193fb7634ef9a
putianxi <putianxi@gmail.com>
2016-07-30 14:31:17 +0800

    finish AreaSelectBox feature

--- src/uielements/AreaSelectBox.vue
+++ src/uielements/AreaSelectBox.vue
@@ -2,16 +2,28 @@
     <div class="control is-grouped" id="select-group">
         <div class="control is-expanded">
             <span class="select is-fullwidth">
-                <select @click="selectArea()">
-                    <option value="">选择省份</option>
+                <select v-model="selectedProvince"
+                        :class="{ 'is-disabled': provinceList.length === 0 }"
+                        @change="onChangeProvince()"
+                >
+                    <option selected value="all">全部省份</option>
+                    <option v-for="option of provinceList" :value="option">
+                        {{ option }}
+                    </option>
                 </select>
             </span>
         </div>
 
         <div class="control is-expanded">
             <span class="select is-fullwidth">
-                <select>
-                    <option value="">选择城市</option>
+                <select v-model="selectedCity"
+                        :class="{ 'is-disabled': cityList.length === 0 }"
+                        @change="emitAreaChnage()"
+                >
+                    <option selected value="all">全部地区</option>
+                    <option v-for="option of cityList" :value="option">
+                        {{ option }}
+                    </option>
                 </select>
             </span>
         </div>
@@ -21,12 +33,74 @@
 <script>
     import messageBus from '../utilities/messageBus.js';
 
+    const direct_city = ['北京市', '天津市', '上海市', '重庆市'];
+
     export default {
-        methods: {
-            selectArea() {
-                console.log('emit');
-                messageBus.$emit('id-selected', '中国');
+        data() {
+            return {
+                // page element
+                provinceList: [],
+                cityList: [],
+                selectedProvince: null,
+                selectedCity: null,
+                // variable
+                china_area: {},
+                china_area_url: '../../assets/data/china_area.json',
             }
+        },
+        methods: {
+            fetchChianList(url) {
+                this.$http.get(url).then((response) => {
+                    this.generateChinaArea(response.json());
+                });
+            },
+            generateChinaArea(china_area_data) {
+                // parse chian area data
+                for(let el of china_area_data) {
+                    // parse province
+                    let province_name = el['province'];
+                    let city_list = []
+
+                    // 不属于直辖市
+                    if (direct_city.indexOf(province_name) === -1) {
+                        // parse city list
+                        city_list = Object.keys(el['city']);
+                    }
+                    else {
+                        city_list = el['city'][province_name];
+                    }
+
+                    // fill this.chianArea
+                    this.china_area[province_name] = city_list;
+                }
+                // fill this.provinceList
+                this.provinceList = Object.keys(this.china_area);
+            },
+            onChangeProvince() {
+                // restore city select
+                this.selectedCity = 'all';
+
+                // refill city_list
+                if( this.selectedProvince === 'all') {
+                    this.cityList = [];
+                }
+                else {
+                    this.cityList = this.china_area[this.selectedProvince];
+                }
+
+                // notify area change
+                this.emitAreaChnage()
+            },
+            emitAreaChnage() {
+                let args = {
+                    province: this.selectedProvince,
+                    city: this.selectedCity,
+                }
+                messageBus.$emit('select-area-change', args);
+            }
+        },
+        ready() {
+            this.fetchChianList(this.china_area_url);
         }
     }
 
